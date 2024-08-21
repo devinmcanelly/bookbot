a331cdc50b48e60116ecf51efc7b8186b85cc146
devinmcanelly <devinmcanelly@outlook.com>
2024-08-21 12:18:16 -0500

    word count and letter count working. TODO: sort count

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ def main():
         for w in words:
             i+=1
         return i
-    i = word_count(file_contents)
+    
+    def letter_count(file_contents):
+        lowered = file_contents.lower()
+        count = {}
+        for l in lowered:
+            if l.isalpha() == True:
+                count[l] = count.get(l, 0) + 1
+        return count
+    
+    # def sort(count):
+    #     return count[]
+    letters = letter_count(file_contents)
+    words = word_count(file_contents)
+    print(letters)
     
 
 main()
