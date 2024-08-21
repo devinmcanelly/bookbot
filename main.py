c8d05e1fc5a69ab43d1061042a8dcb925e52d4a8
devinmcanelly <devinmcanelly@outlook.com>
2024-08-21 01:52:20 -0500

    word count working

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+
+    def word_count(file_contents):
+        words = file_contents.split()
+        i = 0
+        for w in words:
+            i+=1
+        return i
+    i = word_count(file_contents)
+    
+
 main()
