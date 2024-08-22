df1d8643de7d85cd641f5b87411e000dc6044a35
devinmcanelly <devinmcanelly@outlook.com>
2024-08-22 01:07:18 -0500

    Display function is... better. Refactored code to use less functions. I could be using better variables.

--- main.py
+++ main.py
@@ -1,58 +1,40 @@
 with open("books/frankenstein.txt") as f:
         file_contents = f.read() ## Open book and load it into a variable.
 
-def word_count(file_contents):
-    ''' 
-    Takes a given string (file_contents) and returns the word count as an int. 
-    '''
-    words = file_contents.split()
-    i = 0
-    for w in words:
-        i+=1
-    return i
-
-
 def letter_count(file_contents):
     '''
     Takes a given string, and returns a dictionary containing {letter: count} where count is the number of times
-    that character appears in the text.
+    that character appears in the text, and the len() for a word count.
     '''
     lowered = file_contents.lower()
     count = {}
     for l in lowered:
-        if l.isalpha() == True:
+        if l.isalpha():
             count[l] = count.get(l, 0) + 1
-    return count
-
-def sort_letters(count):
-    '''
-    This should probably just be what letter_count returns.
-    It's a list of dictionaries like [{"letter": a, "val": 1234 }, ...] where val is the character count.
-    '''
-    sorted_char = []
+    sorted = []
     for c in count:
-        sorted_char.append({"letter": c, "val": count[c]})
+        sorted.append({"letter": c, "val": count[c]})
     
     def sort_on(item):
         return item["val"]
     
-    sorted_char.sort(reverse=True, key=sort_on)
-    return sorted_char
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted, len(lowered.split())
 
 
 def printout(wc,chars):
     line = "================================================================================\n"
-
-    print(f'frankenstein has {wc} words.\n' + line )
-    print(f'{chars}')
+    short_line = "==============="
+    print(line + f'frankenstein has {wc} words.\n' + short_line + short_line )
+    for c in chars:
+        print(f'{c["letter"]} was used {c["val"]} times')
+        print(short_line)
 
 
 
 def main():
-    wc = word_count(file_contents)
-    chars = letter_count(file_contents)
-    chars_sorted = sort_letters(chars)
-    printout(wc,chars_sorted)
+    chars, wc = letter_count(file_contents)
+    printout(wc,chars)
 
 
 main()
